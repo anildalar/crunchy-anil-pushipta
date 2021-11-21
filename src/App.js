cd74f82af6ebe78beab20a0d5fa264962281c0d4
SuperAnil1996 <papa143amit@gmail.com>
2021-11-21 14:41:09 +0530

    work start with error nd ore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState} from "react";
-import { Route,Switch } from "react-router-dom";
+import { Route,Switch } from "react-router";
 import { useSelector } from "react-redux";
 //import {useSelector, useDispatch} from ""
 import Domain from "./pages/Domain";
